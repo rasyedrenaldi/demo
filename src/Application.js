2d1d7413e83600db09c4d1daf6c17612af892a7e
Rasyid-deve <renaldirasyed@gmail.com>
2024-01-15 15:09:15 +0700

    update state component

--- src/Application.js
+++ src/Application.js
@@ -5,17 +5,27 @@ class Application extends Component {
     super(props);
     this.state = {
       count: 0,
+      overTen: false,
     };
   }
   handleClick = () => {
-    console.log('Clicked');
+    this.setState({ count: this.state.count + 1 });
   };
+
+  componentDidUpdate(props, state) {
+    if (this.state.count > 10 && this.state.count !== state.count && !this.state.overTen) {
+      console.log('Updating over ten');
+      this.setState({ overTen: true });
+    }
+  }
 
   render() {
     let { count } = this.state;
     return (
       <div>
-        <h3>clicked the button {count} times</h3>
+        <h1>clicked the button {count} times</h1>
+        {this.state.overTen ? <h3>Beat high score of 10!</h3> : null}
+
         <span>
           <button onClick={() => this.handleClick()}>Click This</button>
         </span>
